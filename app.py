0e9ab4a3b65a4a685596bd1cd43ce69eeee02267
Farhan <267093537+eligzer@users.noreply.github.com>
2026-03-14 19:19:38 +0530

    fix static files serving

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ limiter = Limiter(
 
 def create_app():
 
-    app = Flask(__name__, static_folder="static")
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
     app.config.from_object(Config)
 
   
