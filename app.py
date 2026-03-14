85825f0cfc36b2cb1f61cbc31665d78b55f530e4
Farhan <267093537+eligzer@users.noreply.github.com>
2026-03-14 19:32:45 +0530

    fix base template

--- app.py
+++ app.py
@@ -33,9 +33,14 @@ limiter = Limiter(
 
 def create_app():
 
-    app = Flask(__name__, static_folder="static", static_url_path="/static")
-    app.config.from_object(Config)
+    app = Flask(
+        __name__,
+        template_folder="templates",
+        static_folder="static",
+        static_url_path="/static"
+    )
 
+    app.config.from_object(Config)
   
     
 
