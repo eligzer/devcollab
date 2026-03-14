c99f8075e72cbd6bf1c5edffed593b5fb6934d89
Farhan <267093537+eligzer@users.noreply.github.com>
2026-03-14 18:09:47 +0530

    Improve forms validation and profile system

--- forms.py
+++ forms.py
@@ -1,109 +1,295 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, TextAreaField, SelectField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError, NumberRange
+from wtforms import (
+    StringField,
+    PasswordField,
+    TextAreaField,
+    SelectField,
+    SubmitField,
+    IntegerField
+)
+from wtforms.validators import (
+    DataRequired,
+    Email,
+    EqualTo,
+    Length,
+    ValidationError,
+    NumberRange,
+    Optional,
+    URL
+)
+
 from models import User, InviteCode
 
 
+# ----------------------------
+# Login
+# ----------------------------
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=80)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
+
+    username = StringField(
+        "Username",
+        validators=[DataRequired(), Length(min=3, max=80)]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField("Sign In")
 
 
+# ----------------------------
+# Register
+# ----------------------------
 class RegisterForm(FlaskForm):
-    invite_code = StringField('Invite Code', validators=[DataRequired(), Length(min=1, max=40)])
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=80)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    password2 = PasswordField('Confirm Password',
-                              validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
-    submit = SubmitField('Create Account')
+
+    invite_code = StringField(
+        "Invite Code",
+        validators=[DataRequired(), Length(min=1, max=40)]
+    )
+
+    username = StringField(
+        "Username",
+        validators=[DataRequired(), Length(min=3, max=80)]
+    )
+
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), Length(min=6)]
+    )
+
+    password2 = PasswordField(
+        "Confirm Password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match.")
+        ]
+    )
+
+    submit = SubmitField("Create Account")
 
     def validate_username(self, field):
+
         if User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username already taken.')
+            raise ValidationError("Username already taken.")
 
     def validate_email(self, field):
+
         if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+            raise ValidationError("Email already registered.")
 
     def validate_invite_code(self, field):
+
         code = InviteCode.query.filter_by(code=field.data.strip()).first()
+
         if not code:
-            raise ValidationError('Invalid invite code.')
+            raise ValidationError("Invalid invite code.")
+
         if not code.is_active:
-            raise ValidationError('This invite code has been deactivated.')
+            raise ValidationError("This invite code has been deactivated.")
+
         if code.used_by:
-            raise ValidationError('This invite code has already been used.')
+            raise ValidationError("This invite code has already been used.")
 
 
+# ----------------------------
+# Project
+# ----------------------------
 class ProjectForm(FlaskForm):
-    name = StringField('Project Name', validators=[DataRequired(), Length(min=1, max=140)])
-    description = TextAreaField('Description', validators=[Length(max=2000)])
-    submit = SubmitField('Create Project')
+
+    name = StringField(
+        "Project Name",
+        validators=[DataRequired(), Length(min=1, max=140)]
+    )
+
+    description = TextAreaField(
+        "Description",
+        validators=[Length(max=2000)]
+    )
+
+    submit = SubmitField("Create Project")
 
 
+# ----------------------------
+# Code Snippet
+# ----------------------------
 LANGUAGE_CHOICES = [
-    ('python', 'Python'), ('javascript', 'JavaScript'), ('typescript', 'TypeScript'),
-    ('java', 'Java'), ('csharp', 'C#'), ('cpp', 'C++'), ('c', 'C'),
-    ('go', 'Go'), ('rust', 'Rust'), ('ruby', 'Ruby'), ('php', 'PHP'),
-    ('swift', 'Swift'), ('kotlin', 'Kotlin'), ('html', 'HTML'),
-    ('css', 'CSS'), ('sql', 'SQL'), ('bash', 'Bash'), ('json', 'JSON'),
-    ('yaml', 'YAML'), ('xml', 'XML'), ('markdown', 'Markdown'),
+    ("python","Python"),
+    ("javascript","JavaScript"),
+    ("typescript","TypeScript"),
+    ("java","Java"),
+    ("csharp","C#"),
+    ("cpp","C++"),
+    ("c","C"),
+    ("go","Go"),
+    ("rust","Rust"),
+    ("ruby","Ruby"),
+    ("php","PHP"),
+    ("swift","Swift"),
+    ("kotlin","Kotlin"),
+    ("html","HTML"),
+    ("css","CSS"),
+    ("sql","SQL"),
+    ("bash","Bash"),
+    ("json","JSON"),
+    ("yaml","YAML"),
+    ("xml","XML"),
+    ("markdown","Markdown"),
 ]
 
 
 class CodeSnippetForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=140)])
-    language = SelectField('Language', choices=LANGUAGE_CHOICES, default='python')
-    description = TextAreaField('Description', validators=[Length(max=2000)])
-    code = TextAreaField('Code', validators=[DataRequired()])
-    submit = SubmitField('Share Snippet')
+
+    title = StringField(
+        "Title",
+        validators=[DataRequired(), Length(min=1, max=140)]
+    )
+
+    language = SelectField(
+        "Language",
+        choices=LANGUAGE_CHOICES,
+        default="python"
+    )
+
+    description = TextAreaField(
+        "Description",
+        validators=[Length(max=2000)]
+    )
+
+    code = TextAreaField(
+        "Code",
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField("Share Snippet")
 
 
+# ----------------------------
+# Class Notes
+# ----------------------------
 class ClassNoteForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=200)])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Save Note')
+
+    title = StringField(
+        "Title",
+        validators=[DataRequired(), Length(min=1, max=200)]
+    )
+
+    content = TextAreaField(
+        "Content",
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField("Save Note")
 
 
+# ----------------------------
+# Comments
+# ----------------------------
 class CommentForm(FlaskForm):
-    content = TextAreaField('Comment', validators=[DataRequired(), Length(min=1, max=5000)])
-    submit = SubmitField('Post Comment')
+
+    content = TextAreaField(
+        "Comment",
+        validators=[DataRequired(), Length(min=1, max=5000)]
+    )
+
+    submit = SubmitField("Post Comment")
 
 
+# ----------------------------
+# Invite Codes
+# ----------------------------
 class InviteCodeForm(FlaskForm):
-    count = IntegerField('Number of Codes', validators=[DataRequired(), NumberRange(min=1, max=50)], default=1)
-    submit = SubmitField('Generate Codes')
+
+    count = IntegerField(
+        "Number of Codes",
+        validators=[DataRequired(), NumberRange(min=1, max=50)],
+        default=1
+    )
+
+    submit = SubmitField("Generate Codes")
 
 
+# ----------------------------
+# Edit Profile
+# ----------------------------
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=80)])
-    bio = TextAreaField('About Me', validators=[Length(max=500)])
-    github_link = StringField('GitHub Profile Link', validators=[Length(max=200)])
-    profile_image = FileField('Update Profile Picture', validators=[
-        FileAllowed(['jpg', 'jpeg', 'png'], 'Images only (jpg, jpeg, png).')
-    ])
-    submit = SubmitField('Update Profile')
+
+    username = StringField(
+        "Username",
+        validators=[DataRequired(), Length(min=3, max=80)]
+    )
+
+    bio = TextAreaField(
+        "About Me",
+        validators=[Optional(), Length(max=500)]
+    )
+
+    github_link = StringField(
+        "GitHub Profile",
+        validators=[Optional(), URL(), Length(max=200)]
+    )
+
+    profile_image = FileField(
+        "Profile Picture",
+        validators=[
+            FileAllowed(
+                ["jpg", "jpeg", "png", "gif"],
+                "Images only (jpg, jpeg, png, gif)."
+            )
+        ]
+    )
+
+    submit = SubmitField("Update Profile")
 
     def __init__(self, original_username, *args, **kwargs):
+
         super(EditProfileForm, self).__init__(*args, **kwargs)
+
         self.original_username = original_username
 
     def validate_username(self, field):
+
         if field.data != self.original_username:
+
             user = User.query.filter_by(username=field.data).first()
+
             if user:
-                raise ValidationError('Please use a different username.')
+                raise ValidationError("Please use a different username.")
 
 
+# ----------------------------
+# Search
+# ----------------------------
 class SearchForm(FlaskForm):
-    q = StringField('Search', validators=[DataRequired()])
-    submit = SubmitField('Search')
+
+    q = StringField(
+        "Search",
+        validators=[DataRequired()]
+    )
+
+    submit = SubmitField("Search")
 
 
+# ----------------------------
+# Custom Profile Links
+# ----------------------------
 class UserLinkForm(FlaskForm):
-    title = StringField('Link Title', validators=[DataRequired(), Length(max=100)])
-    url = StringField('URL', validators=[DataRequired(), Length(max=500)])
-    submit = SubmitField('Add Link')
+
+    title = StringField(
+        "Link Title",
+        validators=[DataRequired(), Length(max=100)]
+    )
+
+    url = StringField(
+        "URL",
+        validators=[DataRequired(), URL(), Length(max=500)]
+    )
+
+    submit = SubmitField("Add Link")
