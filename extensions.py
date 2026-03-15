9788c68bd04f74fdd6bd2632040081b1143d1645
Farhan <267093537+eligzer@users.noreply.github.com>
2026-03-15 12:46:13 +0530

    Stabilize socketio threading mode for deployement

--- extensions.py
+++ extensions.py
@@ -3,8 +3,6 @@ from flask_socketio import SocketIO
 socketio = SocketIO(
     cors_allowed_origins="*",
     async_mode="threading",
-    logger=True,
-    engineio_logger=True
-)
+    
 
 online_users = set()
